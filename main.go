40aea35dc411ca8426b8ac1ce567d035342eb547
geonwoo <kgw7401@gmail.com>
2023-09-09 13:56:40 +0900

    ✨ add:  urlChecker with goroutine

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-
-	mydict "github.com/kgw7401/learngo/mydict"
+	"net/http"
 )
 
 // // Account
@@ -19,48 +18,100 @@ import (
 // 	fmt.Println(account)
 // }
 
+// func main() {
+// 	dictionary := mydict.Dictionary{"first": "First word"}
+// 	// Search
+// 	definition, err := dictionary.Search("second")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		fmt.Println(definition)
+// 	}
+
+// 	// Add
+// 	word := "hello"
+// 	definition := "Greeting"
+// 	err := dictionary.Add(word, definition)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	hello, _ := dictionary.Search("hello")
+// 	fmt.Println("found", word, "definition:", hello)
+// 	err2 := dictionary.Add(word, definition)
+// 	if err2 != nil {
+// 		fmt.Println(err2)
+// 	}
+
+// 	// Update
+// 	baseWord := "hello"
+// 	dictionary.Add(baseWord, "First")
+// 	err := dictionary.Update(baseWord, "Second")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	word, _ := dictionary.Search(baseWord)
+// 	fmt.Println(word)
+
+// 	// Delete
+// 	baseWord := "hello"
+// 	dictionary.Add(baseWord, "First")
+// 	dictionary.Search(baseWord)
+// 	dictionary.Delete(baseWord)
+// 	word, err := dictionary.Search(baseWord)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(word)
+// }
+
+// // Go Routines & Channels
+// func main() {
+// 	c := make(chan string)
+// 	people := [2]string{"geonwoo", "minwoo"}
+// 	for _, person := range people {
+// 		go isSexy(person, c)
+// 	}
+// 	fmt.Println("Waiting for messages...")
+// 	for i := 0; i < len(people); i++ {
+// 		fmt.Println("Receive this message:", <-c)
+// 	}
+// 	// fmt.Println(<-c) // Error! Becuase there is no more channel
+// }
+
+// func isSexy(person string, c chan string) {
+// 	time.Sleep(time.Second * 5)
+// 	c <- person + " is sexy"
+// }
+
+// URL Checker
+type result struct {
+	url    string
+	status string
+}
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	// // Search
-	// definition, err := dictionary.Search("second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(definition)
-	// }
-
-	// // Add
-	// word := "hello"
-	// definition := "Greeting"
-	// err := dictionary.Add(word, definition)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// hello, _ := dictionary.Search("hello")
-	// fmt.Println("found", word, "definition:", hello)
-	// err2 := dictionary.Add(word, definition)
-	// if err2 != nil {
-	// 	fmt.Println(err2)
-	// }
-
-	// // Update
-	// baseWord := "hello"
-	// dictionary.Add(baseWord, "First")
-	// err := dictionary.Update(baseWord, "Second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// word, _ := dictionary.Search(baseWord)
-	// fmt.Println(word)
-
-	// Delete
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	results := make(map[string]string)
+	c := make(chan result)
+	urls := []string{"https://www.naver.com/", "https://www.google.com/", "https://www.amazon.com/", "https://www.facebook.com/", "https://www.instagram.com/"}
+	for _, url := range urls {
+		go hitURL(url, c)
 	}
-	fmt.Println(word)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- result) { // chac<- : send only channel
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
 }
